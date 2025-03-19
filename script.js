8a7869e9cc5e5a0549ba55bc9c702cdaca168d0c
Bno <Breno_live2005@hotmail.com>
2025-03-18 23:02:03 -0300

    parquimetro

--- script.js
+++ script.js
@@ -1,74 +1,47 @@
 
 class parquimetro {
     #horas;
+    #tarifa;
     constructor(valor){
         this.valor = valor;
         this.#horas = 0;
-        this.tarifa = 1;
+        this.#tarifa = 1;
     }
+
     adicionarValor() {
         const valorInserido = parseInt(document.getElementById("valor").value);
-
-        const tempo_estacionamento = (valorInserido / this.tarifa) * 30
-        const troco = valorInserido - (tempo_estacionamento / 30) * tarifa
-        console.log (tempo_estacionamento);
-        console.log (troco);
-
-
-        if (this.tarifa > 4){
-            alert("Máximo de 4 reais (120 minutos)");
+        if(valorInserido >= 1 && valorInserido <= 4){
+            this.valor = valorInserido;
+            this.#horas = (valorInserido / this.#tarifa) * 30;
+            this.atualizarTempoeHora();
             return;
-        }
-        if(valorInserido >= 1){
-        this.valor = valorInserido;
-        this.#horas = this.valor * 30;
-        this.atualizarTempoeHora();
-    } else {
+        } 
+        if (valorInserido > 4){
+            alert("Máximo de 4 reais (120 minutos). Você recebera seu troco");
+            this.valor = valorInserido;
+            this.#horas = 120;
+            this.troco();
+            this.atualizarTempoeHora();
+            return;
+        } else {
             alert("Minimo de 1 real");
+            return;
         }
     }
 
-   get horas(){
-        return this.#horas;
+    troco(){
+        const valorInserido = parseInt(document.getElementById('valor').value);
+        const tarifaMaxima = 4;
+        const tempo_estacionamento = (valorInserido / this.#tarifa) * 30;
+        const troco = (tempo_estacionamento / 30) * this.#tarifa - tarifaMaxima;
+        document.getElementById('troco').textContent = "R$"+troco.toFixed(2).replace('.',',');
     }
 
     atualizarTempoeHora() {
-        document.getElementById("horas").textContent = this.horas + ' minutos';
-        document.getElementById("valorinserido").textContent = 'R$' + this.valor;
+        document.getElementById("horas").textContent = this.#horas + ' minutos';
+        document.getElementById("valorinserido").textContent = 'R$' + this.valor.toFixed(2).replace('.',',');
         document.getElementById("valor").value = '';
     }
 }
 
 const estacionamento = new parquimetro();
-
-
-/* Pegar o valor inserido pelo usuario
-Fazer a conversão do valor para minutos, pensei em algo como valor * 30, no caso 1 real igual 30 minutos
-Retornar na interface o valor e os minutos/horas
-*/
-//1 real = 30 minutos
-//1,50 = 45 minutos
-//2 = 60 minutos
-
-
-
-
-
-
-
-/*
-const obterid = (id) => document.getElementById(id).value;
-
-function testeclique(){
-    const valor = obterid('valor');
-    const minutos = valor * 30;
-    if(valor > 4){
-        document.getElementById('valorinserido').textContent = "Insira até um maximo de  4 reais"
-        document.getElementById('horas').textContent = "Tempo de permanencia máximo de 120 minutos"
-        return;
-    } if (valor < 1){
-        document.getElementById('valorinserido').textContent = "Valor minimo de 1 real"} else{
-    const mostrarValor = document.getElementById('valorinserido').textContent = 'R$' + valor;
-    const mostrarMinutos = document.getElementById('horas').textContent = minutos + " Minutos";}
-}*/
-
